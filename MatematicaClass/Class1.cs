44441ed8f42f710a59ba4f8a900da9ef33880b0f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-22 12:46:58 +0100

    Add project files.

---
+++ MatematicaClass/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MatematicaClass
+{
+    public class Operazioni
+    {
+        public static double Somma(double num1, double num2)
+        {
+            // double Somma = num1 + num2;
+            return num1 + num2;
+        }
+        public static double Sottrazione(double num1, double num2)
+        {
+            //  double Sottrazione = num1 - num2;
+            return num1 - num2;
+        }
+        public static double Divisione(double num1, double num2)
+        {
+            // double  Divisione = num1 / num2;
+            return num1 / num2;
+        }
+        public static double Moltiplicazione(double num1, double num2)
+        {
+            //double Moltiplicazione = num1 * num2;
+            return num1 * num2;
+        }
+    }
+}
+    
---
+++ Matematicatest1/UnitTest1.cs
@@ -0,0 +1,51 @@
+using MatematicaClass;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Matematicatest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+           public static void TestSomma()
+           {
+                double num1 = 3;
+                double num2 = 4;
+                double somma_aspettata = 7;
+                double somma = Operazioni.Somma(num1, num2);
+                Assert.AreEqual(somma_aspettata, somma);
+           }
+        [TestMethod]
+        public static void Test_Sottrazione()
+        {
+            double num1 = 5;
+            double num2 = 3;
+            double differenza_aspettata = 2;
+            double differenza = Operazioni.Sottrazione(num1, num2);
+            Assert.AreEqual(differenza_aspettata, differenza);
+        }
+        [TestMethod]
+        public static void Test_Divisione()
+        {
+            double num1 = 6;
+            double num2 = 4;
+            double divisione_aspettata = 1.5;
+            double divisione = Operazioni.Divisione(num1, num2);
+            Assert.AreEqual(divisione_aspettata, divisione);
+        }
+        [TestMethod]
+        public static void Test_Moltplicazione()
+        {
+            double num1 = 3;
+            double num2 = 4;
+            double prodotto_aspettata = 12;
+            double prodotto = Operazioni.Moltiplicazione(num1, num2);
+            Assert.AreEqual(prodotto_aspettata, prodotto);
+        }
+    }
+}
+
+
+
+
+
